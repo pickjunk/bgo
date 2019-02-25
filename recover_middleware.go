5bda047eafcc14f740574e1c708eafdb0533ba3a
chieveit <javachieveit@163.com>
2019-02-25 18:46:19 +0800

    log unexpected panic

--- recover_middleware.go
+++ recover_middleware.go
@@ -7,6 +7,7 @@ import (
 	httprouter "github.com/julienschmidt/httprouter"
 	ot "github.com/opentracing/opentracing-go"
 	otlog "github.com/opentracing/opentracing-go/log"
+	log "github.com/sirupsen/logrus"
 )
 
 func recoverMiddleware(w http.ResponseWriter, req *http.Request, ps httprouter.Params, next httprouter.Handle) {
@@ -17,6 +18,10 @@ func recoverMiddleware(w http.ResponseWriter, req *http.Request, ps httprouter.P
 			case *BusinessError:
 				w.Write([]byte(t.Error()))
 				return
+			case *log.Entry:
+				// error from Log.Panic, skip logging
+				// because Log.Panic has logged the error when it called
+				err = nil
 			case string:
 				err = errors.New(t)
 			case error:
@@ -29,6 +34,10 @@ func recoverMiddleware(w http.ResponseWriter, req *http.Request, ps httprouter.P
 			span := ot.SpanFromContext(ctx)
 			if span != nil {
 				span.LogFields(otlog.String("event", "error"), otlog.Error(err))
+			}
+
+			if err != nil {
+				Log.Error(err)
 			}
 
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
